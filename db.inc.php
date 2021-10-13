1061e883f992c8cf19914cac8e0e37a01af4128a
pentesterkaly <52979616+pentesterkaly@users.noreply.github.com>
2021-10-14 00:03:05 +0100

    Update db.inc.php

--- db.inc.php
+++ db.inc.php
@@ -3,5 +3,6 @@ session_start();
 //development
 //$con=mysqli_connect('localhost','root','','leave_management_system');
 //live
-$con=mysqli_connect('remotemysql.com','yf2EdWQHaw','LuQSMJrwgo','yf2EdWQHaw');
+//$con=mysqli_connect('remotemysql.com','yf2EdWQHaw','LuQSMJrwgo','yf2EdWQHaw'); - This is the original
+$con=mysqli_connect('sql4.freemysqlhosting.net','sql4444201','kIxM5xHBEB','sql4444201');
 ?>
